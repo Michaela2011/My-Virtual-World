19508b18461bfdca731da2e382cc17b49d4c4833
Michaela2011 <michaela@imperialestates.biz>
2015-10-24 18:09:18 -0400

    Initial Commit Application directory
    - The very first code in c# actually written
    - I will take care of the licensing soon.

---
+++ World/Application/Application.cs
@@ -0,0 +1,20 @@
+﻿/*
+* I will add a license in shortly as I know this needs to be done.
+* Please bare with me.
+*/
+
+using World.Simulation.Base;
+
+namespace World
+{
+    /// <summary>
+    ///     We need to start a class for the World Server
+    /// </summary>
+    public class Application
+    {
+        public static void Main(string[], args)
+        {
+            BaseApplication.BaseMain(args, "", new SimulationBase());
+        }
+    }
+}
---
+++ World/Application/Properties/AssemblyInfo.cs
@@ -0,0 +1,50 @@
+﻿/*
+* I will add a license in shortly as I know this needs to be done.
+* Please bare with me.
+*/
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+/// <summary>
+///     The general information about an assembly is controlled through the following
+///     set of attributes.  By changing these attribute values we are able to modify
+///     the information associated with an assembly.
+/// </summary>
+[assembly: AssemblyTitle("World")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("World")]
+[assembly: AssemblyCopyright("Copyright © 2015-2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+/// <summary>
+/// Setting ComVisible to false makes the types in this assembly not visible 
+/// to COM components.  If you need to access a type in this assembly from 
+/// COM, set the ComVisible attribute to true on that type.
+/// </summary>
+[assembly: ComVisible(false)]
+
+/// <summary>
+/// The following GUID is for the ID of the typelib if this project is exposed to COM
+/// </summary>
+[assembly: Guid("153e1f6a-8ead-4248-a986-83d4a67fb3b9")]
+
+/// <summary>
+/// Version information for an assembly consists of the following four values:
+///
+///      Major Version
+///      Minor Version 
+///      Build Number
+///      Revision
+///
+/// You can specify all the values or you can default the Build and Revision Numbers 
+/// by using the '*' as shown below:
+/// [assembly: AssemblyVersion("1.0.*")]
+/// </summary>
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
